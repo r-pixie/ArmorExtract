9a6ab4753501b0287b228d4aa2fee6909924604e
Pixie <62771068+r-pixie@users.noreply.github.com>
2025-05-23 03:10:14 +0800

    Update itemsadder.py

--- extracts/itemsadder.py
+++ extracts/itemsadder.py
@@ -48,12 +48,11 @@ class ItemsAdder:
                 os.makedirs(os.path.dirname(f"output/itemsadder/textures/models/{layer_rendering[layer]}.png"), exist_ok=True)
                 shutil.copy(file, f"output/itemsadder/textures/models/{layer_rendering[layer]}.png")
 
-                self.furnace_data["items"].setdefault(f"minecraft:{material}".lower(), {}).setdefault("custom_model_data", {})[custom_model_data] = {
-                    "armor_layer": {
-                        "type": armor_type.lower(),
-                        "texture": f"textures/models/{layer_rendering[layer]}",
-                        "auto_copy_texture": False
+                (f"minecraft:{material}".lower(), {}) = [
+                    {
+                        "custom_model_data": custom_model_data,
+                        "sprite": f"textures/models/{layer_rendering[layer]}",                       
                     }
-                }
+                ]
 
         Utils.save_json("output/itemsadder/furnace.json", self.furnace_data)
